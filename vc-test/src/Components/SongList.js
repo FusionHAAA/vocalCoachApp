e598d7b7c4c3856632b4d63a7807272dc0d2d9ef
empower87 <mjamespower87@gmail.com>
2021-02-18 14:48:30 -0800

    css

--- vc-test/src/Components/SongList.js
+++ vc-test/src/Components/SongList.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import ScrollAnimation from 'react-animate-on-scroll'
 import "animate.css/animate.min.css";
 
@@ -10,6 +9,9 @@ function SongList(props) {
   const [songs, setSongs] = useState([]);
   let [song, setSong] = useState('doja cat');
   const [covers, setCovers] = useState('')
+  // let [name, setName] = useState('pick a song')
+  // let [albumArt, setAlbumArt] = useState()
+  let [backgroundAlbum, setBackgroundAlbum] = useState()
 
   useEffect(() => {
     findTheSong();
@@ -23,7 +25,7 @@ function SongList(props) {
       .then((res) => {
         console.log(res);
         setSongs(res.data.data);
-        setCovers(res.data.data[0].album.cover)
+        setBackgroundAlbum(res.data.data[0].album.cover)
       });
   };
 
@@ -31,25 +33,30 @@ function SongList(props) {
     e.preventDefault()
     findTheSong()
   }
-  
+  // const displayChoice = (x) => {
+  //   setName(x.title)
+  //   setAlbumArt(x.artist.picture)
+  //   setBackgroundAlbum(x.album.cover)
+  // }
+
   let albumCovers = []
 
   const showSongs = () => {
     const size = 6
-    const firstSix = songs.slice(0, size).map((eachSong) => {
+    const firstSix = songs.slice(0, size).map((eachSong, index) => {
       albumCovers.push(eachSong.album.cover)
       return (
         <ScrollAnimation 
-        animateIn='pulse'
-        initiallyVisible={true}
-        scrollableParentSelector='#choose-song-id'
-        animatePreScroll={false}>
-
-          <li key={eachSong.id} className="track-bar">
+          animateIn='pulse'
+          initiallyVisible={true}
+          scrollableParentSelector='#choose-song-id'
+          animatePreScroll={false}>
+        
+          <li key={eachSong.id} className="track-bar" tabIndex="1" onClick={() => {props.displayChoice(eachSong, props.num);setBackgroundAlbum(eachSong.album.cover)}}>
             <img src={eachSong.album.cover} />
               <div className="titles">
-                <h5>{eachSong.title}</h5>
-                <h6>{eachSong.album.title}</h6>
+                <p>{eachSong.title}</p>
+                <p>{eachSong.album.title}</p>
               </div>
               <div className="control-buttons">
                 <i className="fas fa-play-circle"></i>
@@ -60,6 +67,7 @@ function SongList(props) {
               </audio>
           </li>
 
+
         </ScrollAnimation>
       );
     })
@@ -67,26 +75,24 @@ function SongList(props) {
   };
 
   let albumBackground = {
-    backgroundImage: `url(${covers})`,
+    backgroundImage: `url(${backgroundAlbum})`,
   }
 
   return (
-    // <div>
-    //   <form onSubmit={findSong}>
-    //       <input type="text" placeholder="" onChange={(e) => setSong(e.target.value)} />
-    //       <button>Submit</button>
-    //   </form> 
-    //   {showSongs()}
-    // </div>
     <div>
       <form className="search-box" onSubmit={findSong}>
           <input type="text" id="search" placeholder="Find a song" onChange={(e) => setSong(e.target.value)} />
       </form>
       <div className="choose-song-list" id="choose-song-id" style={albumBackground}>
           {showSongs()}
-
       </div>
-  </div>
+      {/* <div className="song-choices">
+        <div className="choice-one">
+            <h4>{name}</h4>
+            <img src={albumArt} />
+        </div>
+      </div> */}
+    </div>
   );
 }
 
