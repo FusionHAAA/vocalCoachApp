472f46d54f3e47b222118804cf62bc583d029a87
empower87 <mjamespower87@gmail.com>
2021-02-20 20:19:04 -0800

    roberto is a god

--- vc-test/src/Components/ChooseSong.js
+++ vc-test/src/Components/ChooseSong.js
@@ -10,7 +10,7 @@ function ChooseSong(props) {
     let [title1, setTitle1] = useState('pick a song')
     let [albumArt1, setAlbumArt1] = useState()
     let [artist2, setArtist2] = useState()
-    let [title2, setTitle2] = useState('pick a song 2')
+    let [title2, setTitle2] = useState('pick a song')
     let [albumArt2, setAlbumArt2] = useState()
     let [processSong1,setProcessSong1] = useState();
     let [processSong2,setProcessSong2] = useState();
@@ -86,10 +86,6 @@ async function getSpotifyAnalysis(id,num) {
   }
  
 
-
-  
- 
-
   function getMusicAnalysis(x){
     let useId1 = songId1
     let useId2 = x
@@ -126,11 +122,9 @@ async function getSpotifyAnalysis(id,num) {
                 </div>
                 <div className="transpose-container">
                     {/* <button className="transpose"><b>Transpose</b></button> */}
-
-
                     {showTest}
-                   
-
+                    <h3>or</h3>
+                    <button className="customize-button">Customize</button>
                 </div>
                 <div className="choice-two">
                     <img src={albumArt2} alt= '' />
